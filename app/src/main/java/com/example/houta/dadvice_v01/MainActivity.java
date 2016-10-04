8782f380a1a59cf792ff83c13828178c9408cbac
SpencerTaft <spencerp@uvic.ca>
2016-10-03 21:29:34 -0700

    Randomizer works, does not check for reusing indexes

--- app/src/main/java/com/example/houta/dadvice_v01/MainActivity.java
+++ app/src/main/java/com/example/houta/dadvice_v01/MainActivity.java
@@ -27,17 +27,20 @@ public class MainActivity extends AppCompatActivity {
         //initialize simple dadVice String array here for now
         ArrayList<String> dadVice = new ArrayList<>();
         dadVice.add("DadVice1 \n \n \n \n \n \n \n DadVice2 \n \n \n \n \n \n \n \n \n");
-        dadVice.add("DadVice2 \n \n \n \n \n \n \n DadVice2 \n \n \n \n \n \n \n \n \n");
-        dadVice.add("DadVice3 \n \n \n \n \n \n \n DadVice3 \n \n \n \n \n \n \n \n \n");
-        dadVice.add("DadVice1 \n \n \n \n \n \n \n DadVice1 \n \n \n \n \n \n \n \n \n");
-        dadVice.add("DadVice2 \n \n \n \n \n \n \n DadVice2 \n \n \n \n \n \n \n \n \n");
-        dadVice.add("DadVice3 \n \n \n \n \n \n \n DadVice3 \n \n \n \n \n \n \n \n \n");
-        dadVice.add("DadVice1 \n \n \n \n \n \n \n DadVice1 \n \n \n \n \n \n \n \n \n");
-        dadVice.add("DadVice2 \n \n \n \n \n \n \n DadVice2 \n \n \n \n \n \n \n \n \n");
-        dadVice.add("DadVice3 \n \n \n \n \n \n \n DadVice3 \n \n \n \n \n \n \n \n \n");
-        dadVice.add("DadVice1 \n \n \n \n \n \n \n DadVice1 \n \n \n \n \n \n \n \n \n");
-        dadVice.add("DadVice2 \n \n \n \n \n \n \n DadVice2 \n \n \n \n \n \n \n \n \n");
-        dadVice.add("DadVice3 \n \n \n \n \n \n \n DadVice3 \n \n \n \n \n \n \n \n \n");
+        dadVice.add("DadVice3 \n \n \n \n \n \n \n DadVice4 \n \n \n \n \n \n \n \n \n");
+        dadVice.add("DadVice5 \n \n \n \n \n \n \n DadVice6 \n \n \n \n \n \n \n \n \n");
+        dadVice.add("DadVice7 \n \n \n \n \n \n \n DadVice8 \n \n \n \n \n \n \n \n \n");
+        dadVice.add("DadVice9 \n \n \n \n \n \n \n DadVice10 \n \n \n \n \n \n \n \n \n");
+        dadVice.add("DadVice11 \n \n \n \n \n \n \n DadVice12 \n \n \n \n \n \n \n \n \n");
+        dadVice.add("DadVice13 \n \n \n \n \n \n \n DadVice14 \n \n \n \n \n \n \n \n \n");
+        dadVice.add("DadVice15 \n \n \n \n \n \n \n DadVice16 \n \n \n \n \n \n \n \n \n");
+        dadVice.add("DadVice17 \n \n \n \n \n \n \n DadVice18 \n \n \n \n \n \n \n \n \n");
+        dadVice.add("DadVice19 \n \n \n \n \n \n \n DadVice20 \n \n \n \n \n \n \n \n \n");
+        dadVice.add("DadVice21 \n \n \n \n \n \n \n DadVice22 \n \n \n \n \n \n \n \n \n");
+        dadVice.add("DadVice23 \n \n \n \n \n \n \n DadVice24 \n \n \n \n \n \n \n \n \n");
+
+        //testing randomizer here
+        ArrayList<String> randomized_test_list = randomize_ArrayList(dadVice);
 
         //now, display the dadVice one by one
         //ref: https://developer.android.com/guide/topics/ui/declaring-layout.html
@@ -46,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         //initialize ArrayAdapter
         ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, dadVice);
+                android.R.layout.simple_list_item_1, randomized_test_list);//dadVice);
 
         //initialize ListView
         ListView myListView = (ListView)findViewById(R.id.listView);
@@ -59,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
     This function takes an input arraylist and then randomizes it
     https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html
      */
-    ArrayList<String> randomizer_ArrayList(ArrayList<String> input_array)
+    ArrayList<String> randomize_ArrayList(ArrayList<String> input_array)
     {
         //Input parameters to randomizer
         ArrayList<String> randomized_array = input_array;//used to hold output
