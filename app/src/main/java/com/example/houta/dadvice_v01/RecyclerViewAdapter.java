756a5e4b96072f792f09d98ad82eec79d4d88e45
SpencerTaft <spencerp@uvic.ca>
2016-10-20 12:30:53 -0700

    Added new adapter, no errors but not currently hooked up, added new xml

---
+++ app/src/main/java/com/example/houta/dadvice_v01/RecyclerViewAdapter.java
@@ -0,0 +1,135 @@
+package com.example.houta.dadvice_v01;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.NativeExpressAdView;
+
+import java.util.List;
+
+/**
+ * The {@link RecyclerViewAdapter} class.
+ * <p>The adapter provides access to the items in the {@link DadviceItemViewHolder}
+ * or the {@link NativeExpressAdViewHolder}.</p>
+ */
+class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    // A menu dadvice view type.
+    private static final int DADVICE_VIEW_TYPE = 0;
+
+    // The Native Express ad view type.
+    private static final int NATIVE_EXPRESS_AD_VIEW_TYPE = 1;
+
+    // An Activity's Context.
+    private final Context mContext;
+
+    // The list of Native Express ads and dadvice items.
+    private final List<Object> mRecyclerViewItems;
+
+    /**
+     * For this example app, the recyclerViewItems list contains only
+     * {@link //Dadvice} and {@link //NativeExpressAdView} types.
+     */
+    public RecyclerViewAdapter(Context context, List<Object> recyclerViewItems) {
+        this.mContext = context;
+        this.mRecyclerViewItems = recyclerViewItems;
+    }
+
+    /**
+     * The {@link DadviceItemViewHolder} class.
+     * Provides a reference to each view in the menu item view.
+     */
+    public class DadviceItemViewHolder extends RecyclerView.ViewHolder {
+        private TextView dadvice_text;
+
+        DadviceItemViewHolder(View view) {
+            super(view);
+            dadvice_text = (TextView) view.findViewById(R.id.cardViewId);//TODO check this matches xml properly in card_view.xml
+        }
+    }
+
+    /**
+     * The {@link NativeExpressAdViewHolder} class.
+     */
+    public class NativeExpressAdViewHolder extends RecyclerView.ViewHolder {
+
+        NativeExpressAdViewHolder(View view) {
+            super(view);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecyclerViewItems.size();
+    }
+
+    /**
+     * Determines the view type for the given position.
+     */
+    @Override
+    public int getItemViewType(int position) {
+        return (position % MainActivity.ITEMS_PER_AD == 0) ? NATIVE_EXPRESS_AD_VIEW_TYPE
+                : DADVICE_VIEW_TYPE;
+    }
+
+    /**
+     * Creates a new view for a menu item view or a Native Express ad view
+     * based on the viewType. This method is invoked by the layout manager.
+     */
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        switch (viewType) {
+            case DADVICE_VIEW_TYPE:
+                View DadviceItemLayoutView = LayoutInflater.from(viewGroup.getContext()).inflate(
+                        R.layout.card_view, viewGroup, false);//TODO use card_view_container instead?
+                return new DadviceItemViewHolder(DadviceItemLayoutView);
+            case NATIVE_EXPRESS_AD_VIEW_TYPE:
+                // fall through
+            default:
+                View nativeExpressLayoutView = LayoutInflater.from(
+                        viewGroup.getContext()).inflate(R.layout.native_express_ad_container,
+                        viewGroup, false);
+                return new NativeExpressAdViewHolder(nativeExpressLayoutView);
+        }
+
+    }
+
+    /**
+     *  Replaces the content in the views that make up the menu item view and the
+     *  Native Express ad view. This method is invoked by the layout manager.
+     */
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        int viewType = getItemViewType(position);
+        switch (viewType) {
+            case DADVICE_VIEW_TYPE:
+//holder.mTextView.setText(mDataset.get(position));
+
+                DadviceItemViewHolder DadviceItemHolder = (DadviceItemViewHolder) holder;
+                //MenuItem menuItem = (MenuItem) mRecyclerViewItems.get(position);
+                String dadvice_item = mRecyclerViewItems.get(position).toString();
+
+                DadviceItemHolder.dadvice_text.setText(dadvice_item);
+                break;
+            case NATIVE_EXPRESS_AD_VIEW_TYPE:
+                // fall through
+            default:
+                NativeExpressAdViewHolder nativeExpressHolder =
+                        (NativeExpressAdViewHolder) holder;
+                NativeExpressAdView adView =
+                        (NativeExpressAdView) mRecyclerViewItems.get(position);
+                ViewGroup adCardView = (ViewGroup) nativeExpressHolder.itemView;
+                if (adCardView.getChildCount() > 0) {
+                    adCardView.removeAllViews();
+                }
+
+                // Add the Native Express ad to the native express ad view.
+                adCardView.addView(adView);
+        }
+    }
+
+}
