92b49d4c6247f3121c7bf714a9f970ccecd56368
SpencerTaft <spencerp@uvic.ca>
2016-11-15 20:27:24 -0800

    Added email link

--- app/src/main/java/com/example/houta/dadvice_v01/MainActivity.java
+++ app/src/main/java/com/example/houta/dadvice_v01/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 //import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
